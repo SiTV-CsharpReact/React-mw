f125964d81dcd6d3929a865580e9c2a9d784788a
hiepnxph15148 <97279417+hiepnxph15148@users.noreply.github.com>
2023-05-26 15:18:52 +0700

    update new

--- src/components/menuBarMW/DanhMuc.tsx
+++ src/components/menuBarMW/DanhMuc.tsx
@@ -5,8 +5,6 @@ import  { listStock } from "./codeListSlice";
 import { fetchCategoryAsync } from "./danhmucSlice";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { fetchTableHNXAsync, fetchTableHSXAsync, setProductParams } from "../tableMarketwatch/tableSlice";
-// import { fetchDataTableHNXAsync, } from "../tableMarketwatch/tableSlice";
 
 const DanhMuc = () => {
   const navigate = useNavigate();
@@ -14,51 +12,48 @@ const DanhMuc = () => {
   const { isLoading, data, status } = useSelector(
     (state: RootState) => state.categories
   );
-  
 //  console.log({ isLoading, data, status })
  useEffect(() => {
   dispatch(fetchCategoryAsync());
 }, [dispatch]);
-function handleDispatch(item:string) {
-  dispatch(fetchTableHNXAsync(item))
-  dispatch(fetchTableHSXAsync(item))
+function handleDispatch() {
   // Xử lý logic của dispatch
 
   // Chuyển hướng đến trang chủ
-  // navigate('/chung-khoan/danh-muc');
+  navigate('/chung-khoan/danh-muc');
   
 }
   return (
-    <div className="group list-sub-menu">
-      <span className="text-13px">Danh mục</span>
-      <ul className="absolute hidden text-black group-hover:block z-40 sub-menu">
-      {data && data.Data.map((item: any, index: number) => (
-          <React.Fragment key={index}>
-            <li className="relative">
-              <Link to="/chung-khoan/danh-muc" className=" "  onClick={() => handleDispatch(item.List)}> 
-              {/* <Link to="/chung-khoan/danh-muc" className=" "  onClick={() =>console.log("ok",item.List)}>  */}
-                {item.Name}
-              </Link>  
-              <span id={`btDel${index}`} className="imgDel keep" />
-            </li>
-          </React.Fragment>
-        ))}
-         
-        <li className="relative">
-          <input
-            className="textBox"
-            placeholder="Thêm danh mục"
-            value={newCategory}
-            onChange={(e) => setNewCategory(e.target.value)}
-          />
+    <></>
+  //   <div className="group list-sub-menu">
+  //     <span className="text-13px">Danh mục</span>
+  //     <ul className="absolute hidden text-black group-hover:block z-40 sub-menu">
+  //     {data && data.Data.map((item: any, index: number) => (
+  //         <React.Fragment key={item.Score}>
+  //           <li className="relative">
+  //             <Link to="/chung-khoan/danh-muc" className=" "  onClick={() => dispatch(listStock(item.List))}> 
+  //             {/* <Link to="/chung-khoan/danh-muc" className=" "  onClick={() =>console.log("ok",item.List)}>  */}
+  //               {item.Name}
+  //             </Link>  
+  //             <span id={`btDel${index}`} className="imgDel keep" />
+  //           </li>
+  //         </React.Fragment>
+  //       ))}
+  //       <li className="relative">
+  //         <input
+  //           className="textBox"
+  //           placeholder="Thêm danh mục"
+  //           value={newCategory}
+  //           onChange={(e) => setNewCategory(e.target.value)}
+  //         />
 
-          <span id="btAdd" className="imgAdd keep" onClick={handleAddCategory}>
-            <span></span>
-          </span>
-        </li>
-      </ul>
-    </div>
-  );
+  //         <span id="btAdd" className="imgAdd keep" onClick={handleAddCategory}>
+  //           <span></span>
+  //         </span>
+  //       </li>
+  //     </ul>
+  //   </div>
+   );
 };
 
 export default DanhMuc;
--- src/models/modelTableHNX.ts
+++ src/models/modelTableHNX.ts
@@ -45,3 +45,31 @@ export interface DataTable {
   //   address: IAddress;
   // }
   
+export interface DataGDTT {
+  f8: string
+  f9: string
+  f35: string
+  f49: string
+  f52: string
+  f421: string
+  f422: string
+  f251: string
+  f253: string
+  f252: string
+  f210: any
+  f211: any
+  f220: string
+  f221: string
+  f240: string
+  f241: string
+  f270: string
+  f17: string
+  f425: string
+  f426: string
+  f388: string
+  f399: string
+  f250: string
+  f336: string
+  f340: string
+  f341: string
+}
