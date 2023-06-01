e71360492c6568d378e2c4b1607cb35d3aea464c
thanhdo21d <thanhdo9xi@gmail.com>
2023-06-01 08:27:09 +0700

    update

--- src/components/orderFormMarketwatch/IntradayOrder.tsx
+++ src/components/orderFormMarketwatch/IntradayOrder.tsx
@@ -13,11 +13,9 @@ const IntradayOrder = () => {
   useEffect(() => {
     dataInterday()
   }, [])
-  
   return (
     <div>
       <div className='flex justify-end'>
-
       <PdfandExcel/>
       </div>
       <div  className='mx-4 mt-2'>
@@ -39,28 +37,24 @@ const IntradayOrder = () => {
             <th style={{border:"1px solid #dedede"}} className='font-bold text-black border shadow-sm'>Thông báo</th>
             </tr>
           </thead>
-
-
           <tbody>
             {data.map((item: any, index: any) => {
-              
               return  <tr  style={{border:"1px solid #dedede"}} className='p-2' key={index}>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" , fontSize:"Arial" }}  className='py-1 !text-[13px] !text-left pl-2'>{item.ADATETIME}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !font-[Arial] !text-left pl-1'>{ item.ASTOCKCODE}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-left pl-1'>Lệnh thường	</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-left pl-1 '>{ item.ABUYSELL.toUpperCase() === "S" ? "Bán" : "Mua" }</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-left pl-1 '>{ item.AORDERTYPE_VN}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-right pr-1'>{ item.AQUANTITY}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-right pr-1'> { item.APRICE}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-1'>{ item.AEXCHANGE}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-1'>{ item.AORDERSTATUS_VN }</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-1'>{ item.ATRADINGACCOUNT}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left !pl-1'>{ item.AORIGORDERID}</th>
-              <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }} className='py-1 !text-[13px] !text-left !pl-1 w-[70px]'>{ item.AORIGORDERID}</th>
-                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }} className='py-1 !text-[13px] !text-left !pl-1'>{ item.AMESSAGE_VN}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-2 !font-extralight'>{item.ADATETIME}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !font-[Arial] !text-left pl-1 !font-extralight'>{ item.ASTOCKCODE}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-left pl-1 !font-extralight'>Lệnh thường</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-left pl-1  !font-extralight'>{ item.ABUYSELL.toUpperCase() === "S" ? "Bán" : "Mua" }</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-left pl-1  !font-extralight'>{ item.AORDERTYPE_VN}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px]  !text-right pr-1 !font-extralight'>{ item.AQUANTITY}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-right pr-1 !font-extralight'> { item.APRICE}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-1 !font-extralight'>{ item.AEXCHANGE}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-1 !font-extralight'>{ item.AORDERSTATUS_VN }</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left pl-1 !font-extralight'>{ item.ATRADINGACCOUNT}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }}  className='py-1 !text-[13px] !text-left !pl-1 !font-extralight'>{ item.AORIGORDERID}</th>
+              <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }} className='py-1 !text-[13px] !text-left !pl-1 w-[70px] !font-extralight'>{ item.AORIGORDERID}</th>
+                <th  style={{ color: item.ABUYSELL.toUpperCase() === "S" ? "#9c0A0A" : "#2371AF" , border:"1px solid #dedede" }} className='py-1 !text-[13px] !text-left !pl-1 !font-extralight'>{ item.AMESSAGE_VN}</th>
             </tr>
             })}
-           
           </tbody>
         </table>
       </div>
